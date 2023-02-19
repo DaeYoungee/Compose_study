1326639031af441df56f1ead34bc6dee98be7c34
정대영 <eodud1025@naver.com>
2023-02-20 01:47:51 +0900

    coroutineScope 테스트

--- mygallery/src/main/java/com/example/mygallery/MainActivity.kt
+++ mygallery/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,22 +1,57 @@
 package com.example.mygallery
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SideEffect()
-            LaunchedEffect(Unit){
-
+            val navController = rememberNavController()
+            val scaffoldState = rememberScaffoldState()
+            NavHost(navController = navController, startDestination = "main") {
+                composable(route = "main") {
+                    Scaffold(scaffoldState = scaffoldState) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(it),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Button(onClick = {
+                                navController.navigate("first")
+                            }) {
+                                Text(text = "change composable main")
+                            }
+                            Button(onClick = {
+                                lifecycleScope.launch { scaffoldState.snackbarHostState.showSnackbar("main") }
+                            }) {
+                                Text(text = "show snackbar")
+                            }
+                        }
+                    }
+                }
+                composable(route = "first") {
+                    FirstScreen(navController = navController)
+                }
+                composable(route = "second") {
+                    SecondScreen(navController = navController)
+                }
             }
 
         }
@@ -24,33 +59,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SideEffect() {
-    val (text, setText) = remember {
-        mutableStateOf("initial")
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        TextField(
-            value = text,
-            onValueChange = setText,
+fun RememberCorountione(onChange: () -> Unit, text: String) {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(scaffoldState = scaffoldState) {
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(20.dp))
-        RememberUpdateTestText(text)
+                .fillMaxSize()
+                .padding(it),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                onChange()
+                Log.d("change page", "$onChange")
+            }) {
+                Text(text = "change composable $text")
+            }
+            Button(onClick = {
+                scope.launch { scaffoldState.snackbarHostState.showSnackbar(text) }
+            }) {
+                Text(text = "show snackbar")
+            }
+        }
     }
 }
 
 @Composable
-fun RememberUpdateTestText(text: String) {
-    val rememberText by remember {
-        mutableStateOf(text)
+fun FirstScreen(navController: NavController) {
+    val onChange = {
+        navController.navigate("second")
     }
-    val rememberUpdatedText by rememberUpdatedState(newValue = text)
-    Text(text = "Text : $text")
-    Text(text = "RememberText : $rememberText.value")
-    Text(text = "UpdatedText : ${rememberUpdatedText}")
+    RememberCorountione(onChange = onChange, text = "first page")
+}
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    val onChange = {
+        navController.navigate("main")
+    }
+    RememberCorountione(onChange = onChange, text = "second page")
 }
