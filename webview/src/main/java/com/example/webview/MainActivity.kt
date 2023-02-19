b354a490469983df69a739f6c59b331f59a6736e
정대영 <eodud1025@naver.com>
2023-02-19 22:09:00 +0900

    WebView 뒤로가기 기능 구현

--- webview/src/main/java/com/example/webview/MainActivity.kt
+++ webview/src/main/java/com/example/webview/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.webview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.ColorRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,51 +14,63 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.flow.collectLatest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val viewModel = viewModel<MainViewModel>()
-            HomeScreen(viewModel=viewModel)
+            HomeScreen(viewModel = viewModel)
         }
     }
 }
 
 @Composable
-fun HomeScreen(viewModel:MainViewModel) {
+fun HomeScreen(viewModel: MainViewModel) {
+    val focusManager = LocalFocusManager.current
     val (input, setInput) = rememberSaveable {
         mutableStateOf("")
     }
+    val scaffoldState = rememberScaffoldState()
     Scaffold(
         topBar = {
             TopAppBar(
                 title = { Text(text = "나만의 웹 브라우저") },
                 actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { viewModel.undo() }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
-                            contentDescription = "back"
+                            contentDescription = "back",
+                            tint = Color.White
                         )
                     }
-                    IconButton(onClick = { /*TODO*/ }) {
+                    IconButton(onClick = { viewModel.redo() }) {
                         Icon(
                             imageVector = Icons.Default.ArrowForward,
-                            contentDescription = "forward"
+                            contentDescription = "forward",
+                            tint = Color.White
                         )
                     }
                 }
             )
-        }
+        },
+        scaffoldState = scaffoldState
     ) {
         Column(
             modifier = Modifier
@@ -70,25 +84,54 @@ fun HomeScreen(viewModel:MainViewModel) {
                 label = { Text(text = "https://") },
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                keyboardActions = KeyboardActions(onSearch = {})
+                keyboardActions = KeyboardActions(onSearch = {
+                    viewModel.url.value = input
+                    focusManager.clearFocus()
+                })
             )
             Spacer(modifier = Modifier.height(16.dp))
-            MyWebView(viewModel=viewModel)
+            MyWebView(viewModel = viewModel, scaffoldState = scaffoldState)
         }
     }
 }
 
 @Composable
-fun MyWebView(viewModel:MainViewModel) {
+fun MyWebView(viewModel: MainViewModel, scaffoldState: ScaffoldState) {
+    val webView = rememberWebView()
+
+    LaunchedEffect(Unit) {
+        viewModel.undoSharedFlow.collectLatest {
+            if (webView.canGoBack()) webView.goBack()
+            else scaffoldState.snackbarHostState.showSnackbar("더 이상 뒤로 갈 수 없음")
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.redoSharedFlow.collectLatest {
+            if (webView.canGoForward()) webView.goForward()
+            else scaffoldState.snackbarHostState.showSnackbar("더 이상 앞으로 갈 수 없음")
+        }
+    }
+
     AndroidView(
         modifier = Modifier.fillMaxSize(),
-        factory = {
-            WebView(it).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl("https://google.com")
-            }
-        },  // 처음에 화면에 표기해야되는 뷰객체 인스턴스를 지정
-        update = {}    // factory에서 사용된 context가 update로 전달되어 사용됨
+        factory = { webView },  // 처음에 화면에 표기해야되는 뷰객체 인스턴스를 지정
+        update = { webView ->
+            webView.loadUrl(viewModel.url.value)
+        }    // factory에서 사용된 context가 update로 전달되어 사용됨
     )
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun rememberWebView(): WebView {
+    val context = LocalContext.current
+    val webView = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://google.com")
+        }
+    }
+    return webView
 }
