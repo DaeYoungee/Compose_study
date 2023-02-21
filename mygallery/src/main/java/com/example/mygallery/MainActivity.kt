b1b35595d203d34fb13a591ba36d2e50190effd8
정대영 <eodud1025@naver.com>
2023-02-21 19:59:35 +0900

    permission setting

--- mygallery/src/main/java/com/example/mygallery/MainActivity.kt
+++ mygallery/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,56 +1,55 @@
 package com.example.mygallery
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.webkit.PermissionRequest
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
+import java.util.jar.Manifest
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            val scaffoldState = rememberScaffoldState()
-            NavHost(navController = navController, startDestination = "main") {
-                composable(route = "main") {
-                    Scaffold(scaffoldState = scaffoldState) {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(it),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Button(onClick = {
-                                navController.navigate("first")
-                            }) {
-                                Text(text = "change composable main")
-                            }
-                            Button(onClick = {
-                                lifecycleScope.launch { scaffoldState.snackbarHostState.showSnackbar("main") }
-                            }) {
-                                Text(text = "show snackbar")
-                            }
-                        }
-                    }
+            val (granted, setGranted) = remember {
+                mutableStateOf(false)
+            }
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                    setGranted(isGranted)
+                    Log.d("gandted", "$isGranted")
                 }
-                composable(route = "first") {
-                    FirstScreen(navController = navController)
-                }
-                composable(route = "second") {
-                    SecondScreen(navController = navController)
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.d("gandted", "$granted")
+                setGranted(true)
+            }
+
+            if(granted) {
+                Text(text = "권한이 허용되었습니다.")
+            } else {
+                PermissionRequestScreen {
+                    launcher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
                 }
             }
 
@@ -59,44 +58,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun RememberCorountione(onChange: () -> Unit, text: String) {
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-    Scaffold(scaffoldState = scaffoldState) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = {
-                onChange()
-                Log.d("change page", "$onChange")
-            }) {
-                Text(text = "change composable $text")
-            }
-            Button(onClick = {
-                scope.launch { scaffoldState.snackbarHostState.showSnackbar(text) }
-            }) {
-                Text(text = "show snackbar")
-            }
+fun PermissionRequestScreen(onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "권한이 허용되지 않았습니다.")
+        Button(onClick = onClick) {
+            Text(text = "권한 요청")
         }
     }
-}
-
-@Composable
-fun FirstScreen(navController: NavController) {
-    val onChange = {
-        navController.navigate("second")
-    }
-    RememberCorountione(onChange = onChange, text = "first page")
-}
-
-@Composable
-fun SecondScreen(navController: NavController) {
-    val onChange = {
-        navController.navigate("main")
-    }
-    RememberCorountione(onChange = onChange, text = "second page")
 }
