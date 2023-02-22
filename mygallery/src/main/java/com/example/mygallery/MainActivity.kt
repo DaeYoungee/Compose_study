30ae437eaab5f9e7d3c4cfad3d0d588a7288484e
정대영 <eodud1025@naver.com>
2023-02-22 23:12:44 +0900

    contentProvider 구현

--- mygallery/src/main/java/com/example/mygallery/MainActivity.kt
+++ mygallery/src/main/java/com/example/mygallery/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.mygallery
 
+import android.app.Application
+import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.webkit.PermissionRequest
 import androidx.activity.ComponentActivity
@@ -16,6 +20,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -28,6 +34,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val contentResolver = contentResolver
             val (granted, setGranted) = remember {
                 mutableStateOf(false)
             }
@@ -45,7 +52,7 @@ class MainActivity : ComponentActivity() {
                 setGranted(true)
             }
 
-            if(granted) {
+            if (granted) {
                 Text(text = "권한이 허용되었습니다.")
             } else {
                 PermissionRequestScreen {
@@ -68,5 +75,32 @@ fun PermissionRequestScreen(onClick: () -> Unit) {
         Button(onClick = onClick) {
             Text(text = "권한 요청")
         }
+    }
+}
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val _photoUris = mutableStateOf(emptyList<Uri>())
+    val photoUris:State<List<Uri>> = _photoUris
+    fun fetchPhotos() {
+        val uris = mutableListOf<Uri>()
+
+        getApplication<Application>().contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            "${MediaStore.Images.ImageColumns.DATE_TAKEN} DESC"
+        )?.use { cursor ->
+            val idIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idIndex)
+
+                val contentUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                uris.add(contentUri)
+            }
+        }
+        _photoUris.value = uris
     }
 }
