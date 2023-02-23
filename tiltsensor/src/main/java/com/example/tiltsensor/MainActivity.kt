7c6799cf9d6360d7b39b6246ae4972c8f7922e72
정대영 <eodud1025@naver.com>
2023-02-24 00:54:51 +0900

    가속도sensor, LifecycleEventObserver 구현

--- tiltsensor/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ tiltsensor/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.tiltsensor
 
+import android.app.Application
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -12,38 +18,69 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import androidx.lifecycle.Lifecycle.Event
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = viewModel<MainViewModel>()
-            viewModel.state.observe(
-                this,
-                Observer { Log.d("test", "observe 성공, state: ${viewModel.state.value}") })
-            Column() {
-                Button(onClick = {
-                    viewModel.setState(viewModel.state.value ?: false)
-                }) {
-                    Text(text = "${viewModel.state.value}")
-                }
-            }
+
         }
     }
 }
 
-class MainViewModel : ViewModel() {
-    private val _state = MutableLiveData<Boolean>(false)
-    val state: LiveData<Boolean> = _state
-    fun setState(param: Boolean) {
-        _state.value = !param
+class MainViewModel(application: Application) : AndroidViewModel(application), SensorEventListener,
+    LifecycleEventObserver {
+
+    private val _x = mutableStateOf(0f)
+    val x: State<Float> = _x
+
+    private val _y = mutableStateOf(0f)
+    val y: State<Float> = _y
+
+    private val sensorManager by lazy {
+        application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
+
+    fun registerSensor() {
+        // 센서 등록
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    fun unregisterSensor() {
+        // 센서 해제
+        sensorManager.unregisterListener(this)
+    }
+
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            Log.d(
+                "MainViewModel",
+                "onSensorChanged x: ${event.values[0]}, y: ${event.values[1]}, z: ${event.values[2]}"
+            )
+            _x.value = event.values[0]
+            _y.value = event.values[1]
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //samplingPeroid 변경될 때 함수
+    }
+
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        if (event == Lifecycle.Event.ON_RESUME)  registerSensor()
+        else if(event == Lifecycle.Event.ON_PAUSE) unregisterSensor()
+    }
+
 }
