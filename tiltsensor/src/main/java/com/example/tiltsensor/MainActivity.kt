112e2d62f70c4652f56d21aa15f2aafddf8b02ec
정대영 <eodud1025@naver.com>
2023-02-25 19:35:01 +0900

    tilt 구현

--- tiltsensor/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ tiltsensor/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -2,35 +2,50 @@ package com.example.tiltsensor
 
 import android.app.Application
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.annotation.ColorRes
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.*
 import androidx.lifecycle.Lifecycle.Event
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 화면이 꺼지지 않게 하기
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        // 화면 가로 모드 고정
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
-        setContent {
 
+        lifecycle.addObserver(viewModel)
+
+        setContent {
+            TiltScreen(x = viewModel.x.value, y = viewModel.y.value)
         }
     }
 }
@@ -79,8 +94,46 @@ class MainViewModel(application: Application) : AndroidViewModel(application), S
     }
 
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-        if (event == Lifecycle.Event.ON_RESUME)  registerSensor()
-        else if(event == Lifecycle.Event.ON_PAUSE) unregisterSensor()
+        if (event == Lifecycle.Event.ON_RESUME) {registerSensor()}
+        else if (event == Lifecycle.Event.ON_PAUSE) {unregisterSensor()}
     }
 
+}
+
+@Composable
+fun TiltScreen(x: Float, y: Float) {
+    val xCoord = x * 20
+    val yCoord = y * 20
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val centerX = size.width / 2
+        val centerY = size.height / 2
+        drawCircle(
+            color = Color.Black,
+            radius = 100f,
+            center = Offset(centerX, centerY),
+            style = Stroke()
+        )
+        drawCircle(
+            color = Color.Green,
+            radius = 100f,
+            center = Offset(centerX + xCoord, centerY + yCoord)
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX - 20, centerY),
+            end = Offset(centerX + 20, centerY)
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX, centerY - 20),
+            end = Offset(centerX, centerY + 20)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewScreen() {
+    TiltScreen(1.5f, 1.2f)
 }
